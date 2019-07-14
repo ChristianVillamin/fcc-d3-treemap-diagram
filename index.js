d888aeb3eb338da82eb95d86ecc2e4088b50593d
Christian Villamin <ChristianVillamin31@gmail.com>
2019-07-14 20:15:09 +0800

    refactored

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
-d3.json(
-  'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json'
-).then(dataset => {
-  const svgWidth = 1400;
-  const svgHeight = 700;
+const data =
+  'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json';
+const svgWidth = 1400;
+const svgHeight = 700;
 
-  const container = d3
-    .select('body')
-    .append('div')
-    .attr('id', 'container')
-    .style('position', 'absolute')
-    .style('top', '50%')
-    .style('left', '50%')
-    .style('transform', 'translate(-50%, -50%)')
-    .style('width', `${svgWidth}px`)
-    .style('height', `${svgHeight}px`)
-    .style('box-shadow', '0 0 5px gray');
+d3.select('#container')
+  .style('width', `${svgWidth}px`)
+  .style('height', `${svgHeight}px`);
 
-  const title = d3
-    .select('#container')
-    .append('h1')
-    .attr('id', 'title')
-    .text('Movie Sales')
-    .style('position', 'absolute')
-    .style('left', `${svgWidth / 2}px`)
-    .style('top', '-55px')
-    .style('transform', 'translate(-50%, -50%)');
+d3.select('#title').style('left', `${svgWidth / 2}px`);
+d3.select('#description').style('left', `${svgWidth / 2}px`);
 
-  const description = d3
-    .select('#container')
-    .append('p')
-    .attr('id', 'description')
-    .text('Top 100 Most Sold Video Games Grouped by Platform')
-    .style('position', 'absolute')
-    .style('left', `${svgWidth / 2}px`)
-    .style('top', '-25px')
-    .style('transform', 'translate(-50%, -50%)');
+d3.json(data).then(dataset => {
+  visualize(dataset);
+});
 
+const visualize = dataset => {
+  // SVG
   const svg = d3
     .select('#container')
     .append('svg')
     .attr('class', 'svg-container')
     .attr('width', svgWidth)
-    .attr('height', svgHeight)
-    .style('background-color', 'white');
-
-  // Tooltip
-  const tooltip = d3
-    .select('#container')
-    .append('p')
-    .text('')
-    .attr('id', 'tooltip')
-    .style('position', 'absolute')
-    .style('pointer-events', 'none')
-    .style('visibility', 'hidden');
+    .attr('height', svgHeight);
 
   // Tree Map
   const root = d3
@@ -60,60 +29,27 @@ d3.json(
     .sum(d => d.value)
     .sort((a, b) => b.height - a.height || b.value - a.value);
 
-  const treemap = d3
+  d3
     .treemap()
     .size([svgWidth, svgHeight])
     .paddingInner(1)(root);
 
   const color = d3.scaleOrdinal(d3.schemeAccent);
 
-  svg
-    .selectAll('rect')
-    .data(root.leaves())
-    .enter()
-    .append('rect')
-    .attr('class', 'tile')
-    .attr('data-name', d => d.data.name)
-    .attr('data-category', d => d.data.category)
-    .attr('data-value', d => d.data.value)
-    .attr('x', d => d.x0)
-    .attr('y', d => d.y0)
-    .attr('width', d => d.x1 - d.x0)
-    .attr('height', d => d.y1 - d.y0)
-    .style('stroke', 'white')
-    .style('fill', d => color(d.data.category))
-    .on('mouseover', d => {
-      tooltip
-        .html(
-          `Category: ${d.data.category}
-          <br/>Value: ${d.data.value}`
-        )
-        .attr('id', 'tooltip')
-        .attr('data-value', d.data.value)
-        .style('font-size', '12px')
-        .style('color', 'white')
-        .style('left', `${d.x0}px`)
-        .style('top', `${d.y0}px`)
-        .style('width', `${d.x1 - d.x0}px`)
-        .style('height', `${d.y1 - d.y0}px`)
-        .style('visibility', 'visible');
-    })
-    .on('mouseout', () => tooltip.style('visibility', 'hidden'));
+  // Tooltip
+  const tooltip = d3.select('#tooltip');
 
+  // Selector
   d3.select('#container')
     .selectAll('h4')
     .data(root.leaves())
     .enter()
     .append('h4')
     .text(d => d.data.name)
-    .style('position', 'absolute')
     .style('left', d => `${d.x0}px`)
     .style('top', d => `${d.y0}px`)
     .style('width', d => `${d.x1 - d.x0}px`)
-    .style('height', d => `${d.y1 - d.y0}px`)
-    .style('font-size', '12px')
-    .style('color', '#333')
-    .style('pointer-events', 'none');
+    .style('height', d => `${d.y1 - d.y0}px`);
 
   // Legend
   const legend = d3
@@ -139,11 +75,42 @@ d3.json(
     .enter()
     .append('text')
     .text(d => d.name)
+    .attr('class', 'legend-text')
     .attr('x', (d, i) => 10 + (svgWidth / dataset.children.length) * i)
     .attr('y', 40)
     .attr('width', svgWidth / dataset.children.length)
     .attr('height', 60)
-    .attr('fill', 'white')
-    .style('font-size', '25px')
-    .style('text-shadow', '2px 2px black');
-});
+    .attr('fill', 'white');
+
+  // Draw Treemap
+  svg
+    .selectAll('rect')
+    .data(root.leaves())
+    .enter()
+    .append('rect')
+    .attr('class', 'tile')
+    .attr('data-name', d => d.data.name)
+    .attr('data-category', d => d.data.category)
+    .attr('data-value', d => d.data.value)
+    .attr('x', d => d.x0)
+    .attr('y', d => d.y0)
+    .attr('width', d => d.x1 - d.x0)
+    .attr('height', d => d.y1 - d.y0)
+    .style('stroke', 'white')
+    .style('fill', d => color(d.data.category))
+    .on('mouseover', d => {
+      tooltip
+        .html(
+          `Category: ${d.data.category}
+          <br/>Value: ${d.data.value}`
+        )
+        .attr('id', 'tooltip')
+        .attr('data-value', d.data.value)
+        .style('left', `${d.x0}px`)
+        .style('top', `${d.y0}px`)
+        .style('width', `${d.x1 - d.x0}px`)
+        .style('height', `${d.y1 - d.y0}px`)
+        .style('visibility', 'visible');
+    })
+    .on('mouseout', () => tooltip.style('visibility', 'hidden'));
+};
